3ab067c4e79e26da964b7b1f178f905e66c781b7
matthew-2000 <m.ercolino1@studenti.unisa.it>
2025-06-27 16:32:16 +0200

    Add new materials and prefabs for heated stone interaction

---
+++ Assets/Scripts/Lab/HeatedStoneToggle.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class HeatedStoneToggle : MonoBehaviour
+{
+    [SerializeField] private Color offColor = Color.gray;       // Colore "spento"
+    [SerializeField] private Color onColor = Color.red;         // Colore "acceso" (es. rosso incandescente)
+    [SerializeField] private Renderer targetRenderer;
+
+    private bool isOn = false;
+
+    private void Start()
+    {
+        if (targetRenderer == null)
+            targetRenderer = GetComponent<Renderer>();
+
+        SetStoneColor(offColor);
+    }
+
+    public void ToggleStoneState()
+    {
+        isOn = !isOn;
+        SetStoneColor(isOn ? onColor : offColor);
+    }
+
+    private void SetStoneColor(Color color)
+    {
+        if (targetRenderer != null)
+        {
+            targetRenderer.material.color = color;
+        }
+    }
+}
